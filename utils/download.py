35d49bee6fc1bdc0166e0473913ae6539db1960c
dede1751 <63297263+dede1751@users.noreply.github.com>
2024-11-25 15:04:39 +0100

    Distributed processing

--- utils/download.py
+++ utils/download.py
@@ -1,17 +1,26 @@
 """
 Modified version of the objaverse_json.py script from: https://github.com/simonschlaepfer/BlenderProcObjaverse
+Downloads and processes Objaverse meshes in parallel:
+ - Converts '.glb' to '.obj'
+ - Normalizes mesh to fit into a unit cube
+ - Centers the mesh into the origin
 """
 
+import os # Set env vars before importing libraries
+os.environ["OPENBLAS_NUM_THREADS"] = "1" 
+os.environ["OPENCV_IO_ENABLE_OPENEXR"] = "1"
+
 import json
-import os
 from typing import Optional
 import argparse
 import random
+import multiprocessing
 
-import trimesh
 import numpy as np
+import objaverse
 from tqdm import tqdm
-import objaverse # this is the local version
+import trimesh 
+
 
 def load_existing_json(file_path):
     if os.path.exists(file_path):
@@ -19,32 +28,26 @@ def load_existing_json(file_path):
             return json.load(file)
     return []
 
+
 def save_to_json(file_path, data):
     directory = os.path.dirname(file_path)
     os.makedirs(directory, exist_ok=True)
     with open(file_path, 'w') as file:
         json.dump(data, file, indent=4)
 
-def find_next_available_id(existing_data):
-    used_ids = {item["obj_id"] for item in existing_data}
-    new_id = 0
-    while new_id in used_ids:
-        new_id += 1
-    return new_id
 
 def convert_glb_to_obj(glb_path: str, obj_path: str):
     # Load the mesh or collection of meshes
     try:
         trimesh_mesh = trimesh.load(glb_path)
     except:
-        return None
+        raise Exception(f"Mesh could not be imported. Skipped {glb_path}")
 
     # Check if the loaded object is a scene (which can contain multiple geometries)
     if isinstance(trimesh_mesh, trimesh.Scene):
         for name, geom in trimesh_mesh.geometry.items():
             if not isinstance(geom, trimesh.Trimesh):
-                print(f"Skipping {glb_path} as it is not a valid Trimesh")
-                return None
+                raise Exception(f"Mesh contains unsupported Geometry. Skipped {glb_path}")
 
         merged_mesh = trimesh.util.concatenate(trimesh_mesh.dump())
     else:
@@ -63,50 +66,64 @@ def convert_glb_to_obj(glb_path: str, obj_path: str):
     merged_mesh.export(obj_path, include_texture=True, file_type='obj')
     return norm_scale
 
-def update_json(objaverse_dict, objaverse_base_path, file_path):
-    # Load existing data if it exists
-    existing_data = load_existing_json(file_path)
-    
-    # Create a map for quick lookup of existing objaverse_id
+
+def process_object(obj_details):
+    objaverse_base_path, glb_path, objaverse_id = obj_details
+    github_id = glb_path.split('/')[-2]
+    obj_path = os.path.join(objaverse_base_path, "objs", github_id, objaverse_id, 'model_scaled.obj')
+    os.makedirs(os.path.dirname(obj_path), exist_ok=True)
+
+    try:
+        norm_scale = convert_glb_to_obj(glb_path, obj_path)
+    except Exception as e:
+        return False, e
+
+    return True, {
+        "objaverse_id": objaverse_id,
+        "github_id": github_id,
+        "norm_scale": norm_scale
+    }
+
+
+def update_json(objaverse_dict, objaverse_base_path, json_path, num_workers=32):
+    existing_data = load_existing_json(json_path)
     existing_map = {item["objaverse_id"] for item in existing_data}
 
-    for idx, (objaverse_id, path) in enumerate(tqdm(objaverse_dict.items(), desc="Processing Objects")):
-        if objaverse_id not in existing_map:
-            github_id = path.split('/')[-2]
-            obj_path = os.path.join(objaverse_base_path, "objs", github_id, objaverse_id, 'model_scaled.obj')
-            obj_path_dir = os.path.dirname(obj_path)
-            os.makedirs(obj_path_dir, exist_ok=True)
-    
-            norm_scale = convert_glb_to_obj(path, obj_path)
-            if norm_scale is None:
-                print(f"{path} could not be converted to .obj")
-                continue
-            else:
-                new_id = find_next_available_id(existing_data)
-                existing_data.append({
-                    "obj_id": new_id,
-                    "objaverse_id": objaverse_id,
-                    "github_id": github_id,
-                    "norm_scale": norm_scale
-                })
-    
-                # Save updated data back to the JSON file
-                if idx % 250 == 0:
-                    save_to_json(file_path, existing_data)
+    obj_details = [
+        (objaverse_base_path, glb_path, objaverse_id)
+        for objaverse_id, glb_path in objaverse_dict.items()
+        if objaverse_id not in existing_map
+    ]
+    print(f"Processing {len(obj_details)} objects with {num_workers} workers...")
 
-    save_to_json(file_path, existing_data)
+    with multiprocessing.Pool(num_workers) as pool:
+        results = tqdm(pool.imap_unordered(process_object, obj_details), total=len(obj_details))
+        errors = []
+
+        for idx, (success, data) in enumerate(results):
+            if success:
+                existing_data.append(data)
+                if idx % 250 == 0:
+                    save_to_json(json_path, existing_data)
+            else:
+                errors.append(data)
+
+        save_to_json(json_path, existing_data)
+    
+    for error in errors:
+        print(error)
+    print(f"Skipped a total of {len(errors)} objects.")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Download Objaverse objects and convert them to '.obj' format.")
-    parser.add_argument('--num_objects', type=int, help="Number of objects to download", default=1000)
+    parser.add_argument('--data_path', type=str, help="Path to folder in which the dataset will be saved.")
+    parser.add_argument('--num_objects', type=int, help="Number of objects to download. Deafault is the full list.", default=None)
     parser.add_argument('--num_workers', type=int, help="Number of download processes to instantiate.", default=32)
     parser.add_argument('--list_file', type=str, help="Path to a list of UIDs to download. Default is random UIDs.", default=None)
     args = parser.parse_args()
 
-    os.environ["OPENCV_IO_ENABLE_OPENEXR"]="1"
-    objaverse_base_path = os.path.join(os.environ["TMPDIR"], "datasets", "objaverse")
-    #objaverse_base_path = "/cluster/work/riner/users/asgobbi/datasets/objaverse"
+    objaverse_base_path = os.path.join(args.data_path, "objaverse")
     objaverse_model_json_path = os.path.join(objaverse_base_path, "objaverse_models.json")
     os.makedirs(objaverse_base_path, exist_ok=True)
 
@@ -122,12 +139,12 @@ if __name__ == "__main__":
         with open(args.list_file, "r") as f:
             uids = [uid.strip() for uid in f.readlines()]
     
-    num_objects = min(args.num_objects, len(uids))
-    object_uids = random.sample(uids, num_objects)
+    if args.num_objects is not None:
+        num_objects = min(args.num_objects, len(uids))
+        uids = random.sample(uids, num_objects)
 
-    objects = objaverse.load_objects(uids=object_uids, download_processes=args.num_workers)
-    objects_dict = objaverse.load_objects(uids=object_uids)
+    objects_dict = objaverse.load_objects(uids=uids, download_processes=args.num_workers)
 
     print("Objects loaded, converting to '.obj'.")
-    update_json(objects_dict, objaverse_base_path, objaverse_model_json_path)
+    update_json(objects_dict, objaverse_base_path, objaverse_model_json_path, num_workers=args.num_workers)
     print(f"Objects saved to {objaverse_base_path}")
